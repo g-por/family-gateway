bc2a12b3871a940bfd24c6a7026260dc1ab0ce79
Oleksandra <ol.drohina@gmail.com>
2025-12-15 22:18:43 +0200

    debug 3

--- src/main/java/com/familybudget/gateway/SecurityConfig.java
+++ src/main/java/com/familybudget/gateway/SecurityConfig.java
@@ -1,3 +1,13 @@
+package com.familybudget.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -11,7 +21,7 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.POST, "/api/users/auth/register").permitAll()
                         .pathMatchers(HttpMethod.POST, "/api/users/auth/login").permitAll()
                         .pathMatchers("/actuator/**").permitAll()
-
+                        .pathMatchers(HttpMethod.GET, "/api/gateway/ping").permitAll()
 
                         .anyExchange().authenticated()
                 )
